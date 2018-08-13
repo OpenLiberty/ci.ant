e869f7ca1dae6d6e6a8ac5f99eb351ae856c6243
Charles Tian <ctian@us.ibm.com>
2018-08-13 15:13:28 -0500

    Fix formatting and AppVeyor for Spring Boot thin task

--- src/test/java/net/wasdev/wlp/ant/SpringBootUtilTest.java
+++ src/test/java/net/wasdev/wlp/ant/SpringBootUtilTest.java
@@ -32,78 +32,85 @@ import net.wasdev.wlp.ant.install.InstallLibertyTask;
 import net.wasdev.wlp.common.plugins.util.OSUtil;
 
 public class SpringBootUtilTest {
-	
-	static private final Boolean isWindows = OSUtil.isWindows();
-	
+
+    static private final Boolean isWindows = OSUtil.isWindows();
+
     private SpringBootUtilTask antTask;
-    
+
     private static File wlpDir;
-       
+
     @ClassRule
     public static TemporaryFolder testFolder = new TemporaryFolder();
-	
+
     @BeforeClass
     public static void setupOpenLiberty() throws Exception {
-    	//Install open-liberty kernel 
+        // Install open-liberty kernel
         InstallLibertyTask install = new InstallLibertyTask();
         install.setProject(new Project());
         install.setBaseDir(testFolder.getRoot().getAbsolutePath());
-    	wlpDir = new File(testFolder.getRoot(), "wlp");
+        wlpDir = new File(testFolder.getRoot(), "wlp");
         install.setType("kernel");
         install.execute();
-        
-        //Install springBoot-1.5 feature
+
+        // Install springBoot-1.5 feature
         ProcessBuilder pb = new ProcessBuilder();
-        if (isWindows) {
-            pb.environment().put("EXIT_ALL", "1");
-        }
         Properties sysp = System.getProperties();
         String javaHome = sysp.getProperty("java.home");
         pb.directory(wlpDir);
         pb.environment().put("JAVA_HOME", javaHome);
         pb.redirectErrorStream(true);
-        String installUtility = "bin/installUtility" + (isWindows ? ".bat":""); 
+        String installUtility;
+        if (isWindows) {
+            installUtility = wlpDir + "\\bin\\installUtility.bat";
+            pb.environment().put("EXIT_ALL", "1");
+        } else {
+            installUtility = wlpDir + "/bin/installUtility";
+        }
+
         pb.command(installUtility, "install", "springBoot-1.5", "--acceptLicense");
         Process p = pb.start();
-        /* wait for installUtility to install feature from Liberty repository. 
-    	   Normally takes 5-10 seconds. */ 
-        p.waitFor(); 
-        if (p.exitValue()!=0) {
-        	fail("Test setup failed installing springBoot-1.5 feature.");
+        /*
+         * wait for installUtility to install feature from Liberty repository. Normally
+         * takes 5-10 seconds.
+         */
+        p.waitFor();
+        if (p.exitValue() != 0) {
+            fail("Test setup failed installing springBoot-1.5 feature.");
         }
     }
-    
+
     @Before
     public void setUpNewTask() {
-    	antTask = new SpringBootUtilTask();
-		antTask.setInstallDir(wlpDir.getAbsoluteFile());
+        antTask = new SpringBootUtilTask();
+        antTask.setInstallDir(wlpDir.getAbsoluteFile());
     }
-    
-    
+
     @Test
     public void testOnSpringBootSampleApp() throws Exception {
-    	File app = new File(this.getClass().getResource("/testApp/wasdev.springBoot-1.0-SNAPSHOT.jar").toURI());
-    	File libCache = new File(wlpDir, "usr/shared/resources/libIndexCache");
-    	antTask.setTargetLibCachePath(libCache.getAbsolutePath());
-    	antTask.setSourceAppPath(app.getAbsolutePath());
-    	antTask.setTargetThinAppPath(wlpDir.getAbsolutePath()+"/wasdev.springBoot-1.0-SNAPSHOT.spring");
-    	antTask.execute();
-    	// Check  SpringBootUtil outputs. Just verify that the tool has created _some_  outputs in the expected locations.
-    	// using a stripped down, small jar which will not start but is sufficient to test the repackaging utility  
-    	File thin = new File(wlpDir, "wasdev.springBoot-1.0-SNAPSHOT.spring");
-    	assertTrue("Thin spring boot jar not found", thin.exists());
-    	
-    	String libCacheFilesToVerify[] = {
-    	"09/f2ee1404726a06ddd7beeb061a58c0dfe15d6b7c516542d28b6e3521f5589e/spring-boot-starter-web-1.5.15.RELEASE.jar",
-    	"18/c4a0095d5c1da6b817592e767bb23d29dd2f560ad74df75ff3961dbde25b79/slf4j-api-1.7.25.jar",
-    	"2c/5d9ed201011c4a1bbe1c4d983645f3c68e6db9ed6267066d204cc1d12e4758/spring-boot-starter-1.5.15.RELEASE.jar",
-    	"41/6c5a0c145ad19526e108d44b6bf77b75412d47982cce6ce8d43abdbdbb0fac/jul-to-slf4j-1.7.25.jar",
-    	"f3/9d7ba7253e35f5ac48081ec1bc28c5df9b32ac4b7db20853e5a8e76bf7b0ed/validation-api-1.1.0.Final.jar"};
-    	
-    	for (String p : libCacheFilesToVerify ) {
-    		if (!new File(libCache, p).exists()) {
-    			fail("Did not find expected file " + wlpDir + "/" + p + " in the libIndexCache");
-    		}
-    	}
+        File app = new File(this.getClass().getResource("/testApp/wasdev.springBoot-1.0-SNAPSHOT.jar").toURI());
+        File libCache = new File(wlpDir, "usr/shared/resources/libIndexCache");
+        antTask.setTargetLibCachePath(libCache.getAbsolutePath());
+        antTask.setSourceAppPath(app.getAbsolutePath());
+        antTask.setTargetThinAppPath(wlpDir.getAbsolutePath() + "/wasdev.springBoot-1.0-SNAPSHOT.spring");
+        antTask.execute();
+        // Check SpringBootUtil outputs. Just verify that the tool has created _some_
+        // outputs in the expected locations.
+        // using a stripped down, small jar which will not start but is sufficient to
+        // test the repackaging utility
+        File thin = new File(wlpDir, "wasdev.springBoot-1.0-SNAPSHOT.spring");
+        assertTrue("Thin spring boot jar not found", thin.exists());
+
+        String libCacheFilesToVerify[] = {
+                "09/f2ee1404726a06ddd7beeb061a58c0dfe15d6b7c516542d28b6e3521f5589e/spring-boot-starter-web-1.5.15.RELEASE.jar",
+                "18/c4a0095d5c1da6b817592e767bb23d29dd2f560ad74df75ff3961dbde25b79/slf4j-api-1.7.25.jar",
+                "2c/5d9ed201011c4a1bbe1c4d983645f3c68e6db9ed6267066d204cc1d12e4758/spring-boot-starter-1.5.15.RELEASE.jar",
+                "41/6c5a0c145ad19526e108d44b6bf77b75412d47982cce6ce8d43abdbdbb0fac/jul-to-slf4j-1.7.25.jar",
+                "f3/9d7ba7253e35f5ac48081ec1bc28c5df9b32ac4b7db20853e5a8e76bf7b0ed/validation-api-1.1.0.Final.jar" };
+
+        for (String p : libCacheFilesToVerify) {
+            if (!new File(libCache, p).exists()) {
+                fail("Did not find expected file " + wlpDir + "/" + p + " in the libIndexCache");
+            }
+        }
     }
 }
