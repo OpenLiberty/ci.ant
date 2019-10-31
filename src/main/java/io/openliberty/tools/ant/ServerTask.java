b7034c68ebc689ec09f598f8c82d52fc8ed8df64
Cheryl King <cheryllk@us.ibm.com>
2019-10-31 12:21:28 -0500

    recreate initial error

--- src/main/java/io/openliberty/tools/ant/ServerTask.java
+++ src/main/java/io/openliberty/tools/ant/ServerTask.java
@@ -459,13 +459,13 @@ public class ServerTask extends AbstractTask {
             }
             if (isWindows) {
                 String archivePath = archive.toString();
-                if (archivePath.contains(" ")) {
+                //if (archivePath.contains(" ")) {
                     // command arguments that contain spaces will get surrounded by quotes by ProcessBuilder on Windows, 
                     // so we need to escape the embedded quotes properly
-                    command.add("\"--archive=\\\"" + archivePath + "\\\"\"");
-                } else {
+                //    command.add("\"--archive=\\\"" + archivePath + "\\\"\"");
+                //} else {
                     command.add("--archive=" + "\"" + archivePath + "\"");
-                }
+                //}
             } else {
                 command.add("--archive=" + archive.toString().replaceAll(" ", "\\\\ "));
             }
