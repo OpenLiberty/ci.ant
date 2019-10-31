a6e96d1b2e93b275d2b0abe38312ddceb74df242
Cheryl King <cheryllk@us.ibm.com>
2019-10-31 11:30:16 -0500

    try quoting entire archive option

--- src/main/java/io/openliberty/tools/ant/ServerTask.java
+++ src/main/java/io/openliberty/tools/ant/ServerTask.java
@@ -462,7 +462,7 @@ public class ServerTask extends AbstractTask {
                 if (archivePath.contains(" ")) {
                     // command arguments that contain spaces will get surrounded by quotes by ProcessBuilder on Windows, 
                     // so we need to escape the embedded quotes properly
-                    command.add("--archive=" + "\\\"" + archivePath + "\\\"");
+                    command.add("\"--archive=\\\"" + archivePath + "\\\"\"");
                 } else {
                     command.add("--archive=" + "\"" + archivePath + "\"");
                 }
