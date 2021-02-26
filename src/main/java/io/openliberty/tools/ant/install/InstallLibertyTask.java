742ca2af2002f136ca8a09261b24f23680413bbc
jjvilleg <jjvilleg@us.ibm.com>
2021-02-26 16:25:31 -0600

    Updated boolean so maven doesn't need to be changed

--- src/main/java/io/openliberty/tools/ant/install/InstallLibertyTask.java
+++ src/main/java/io/openliberty/tools/ant/install/InstallLibertyTask.java
@@ -46,6 +46,7 @@ public class InstallLibertyTask extends AbstractTask {
     private boolean offline;
     private boolean useOpenLiberty;
     
+    private boolean skipAlreadyInstalledCheck = false;
 
     @Override
     public void execute() throws BuildException {
@@ -59,6 +60,14 @@ public class InstallLibertyTask extends AbstractTask {
     private void doExecute() throws Exception {
         if (baseDir == null) {
             baseDir = ".";
+        }
+
+        if(!skipAlreadyInstalledCheck) {
+            File wlpDir = new File(baseDir, "wlp");
+            if (wlpDir.exists()) {
+                log("Liberty profile is already installed.");
+                return;
+            }
         }
 
         if (cacheDir == null) {
@@ -243,5 +252,13 @@ public class InstallLibertyTask extends AbstractTask {
     
     public boolean getUseOpenLiberty() {
         return useOpenLiberty;
+    }
+
+    public void setSkipAlreadyInstalledCheck(boolean skipAlreadyInstalledCheck) {
+        this.skipAlreadyInstalledCheck = skipAlreadyInstalledCheck;
+    }
+
+    public boolean getSkipAlreadyInstalledCheck() {
+        return skipAlreadyInstalledCheck;
     }
 }
