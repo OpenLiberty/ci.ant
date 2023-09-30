7eda2e9bc01c52078ca7f1573241cd27a073ef76
Cheryl King <cheryllk@us.ibm.com>
2023-09-30 16:41:46 -0500

    Change cacheDir and set skipExisting to true

--- src/main/java/io/openliberty/tools/ant/install/InstallLibertyTask.java
+++ src/main/java/io/openliberty/tools/ant/install/InstallLibertyTask.java
@@ -71,7 +71,7 @@ public class InstallLibertyTask extends AbstractTask {
         }
 
         if (cacheDir == null) {
-            File dir = new File(System.getProperty("java.io.tmpdir"), "wlp-cache");
+            File dir = new File(System.getProperty("java.io.tmpdir"), "wlp-cache-"+Long.toString(System.nanoTime()));
             cacheDir = dir.getAbsolutePath();
         }
 
@@ -123,7 +123,7 @@ public class InstallLibertyTask extends AbstractTask {
         get.setUsername(username);
         get.setPassword(password);
         get.setMaxTime(maxDownloadTime);
-        get.setRetries(5);
+        get.setSkipExisting(true);
         get.doGet(source, dest, Project.MSG_INFO, progress);
     }
 
